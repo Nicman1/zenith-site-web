2642e8b2aab02600cd1af8d035349d96d79d1e36
Nicman1 <nic.champagne@outlook.com>
2025-05-25 19:53:10 -0400

    added main page

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {HeaderComponent} from '../header/header.component';
 import {CrashCountdownComponent} from '../crash-countdown/crash-countdown.component';
+import {MainComponent} from '../main/main.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, CrashCountdownComponent],
+  imports: [RouterOutlet, HeaderComponent, CrashCountdownComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
