e3bb0e9bbc86d88fe8bed74bd7c728eb99f75e4b
Nicman1 <nic.champagne@outlook.com>
2025-05-25 19:41:51 -0400

    folder rearrangement and crash countdown

---
+++ src/app/crash-countdown/crash-countdown.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crash-countdown',
+  imports: [],
+  templateUrl: './crash-countdown.component.html',
+  styleUrl: './crash-countdown.component.css'
+})
+export class CrashCountdownComponent {
+
+}
