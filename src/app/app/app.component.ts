c4007b2ca0175dd9c5cc029f84ea1f53859a4612
Nicman1 <nic.champagne@outlook.com>
2025-05-25 20:11:16 -0400

    added footer

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -3,10 +3,11 @@ import {RouterOutlet} from '@angular/router';
 import {HeaderComponent} from '../header/header.component';
 import {CrashCountdownComponent} from '../crash-countdown/crash-countdown.component';
 import {MainComponent} from '../main/main.component';
+import {FooterComponent} from '../footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, CrashCountdownComponent, MainComponent],
+  imports: [RouterOutlet, HeaderComponent, CrashCountdownComponent, MainComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
