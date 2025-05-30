e3f929140a795a965fe39a80521cc8a513af26e4
Nicman1 <nic.champagne@outlook.com>
2025-05-29 22:49:29 -0400

    ta m're

--- src/app/pages/sponsors/sponsors.component.ts
+++ src/app/pages/sponsors/sponsors.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sponsors',
-  imports: [],
+    imports: [],
   templateUrl: './sponsors.component.html',
   styleUrl: './sponsors.component.css'
 })
